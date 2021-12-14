3bbb2e0b49f2cba362b1aa703a8b4c19bed92622
gabi brown <gbrown@lcs.on.ca>
2021-12-14 14:42:58 -0500

    Initial Commit

---
+++ DontSpeedApp/DontSpeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontSpeedAppApp.swift
+//  DontSpeedApp
+//
+//  Created by gabi brown on 2021-12-14.
+//
+
+import SwiftUI
+
+@main
+struct DontSpeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
