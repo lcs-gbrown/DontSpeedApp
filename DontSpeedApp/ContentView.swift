d81b6d1de4145f9b0b3d0d2fb2189ad443429ad2
gabi brown <gbrown@lcs.on.ca>
2021-12-14 15:27:16 -0500

    created values and sliders for speedLimit and speedTravelled

--- DontSpeedApp/ContentView.swift
+++ DontSpeedApp/ContentView.swift
@@ -8,9 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var speedTravelled = 0.0
+    
+    @State var speedLimit = 0.0
+    
+    // MARK: Computed properties
+    var speedDifference: Double {
+        return speedTravelled - speedLimit
+    }
+    
+    var feedback: String {
+        switch speedTravelled {
+        case ...(0):
+            return "You are going under the speed limit"
+        case 1...20:
+                return "$100 fine"
+        case 21...30:
+                return "$270 fine"
+        case 30...150:
+                return "$500 fine"
+        default:
+            return "Travel safe."
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            HStack {
+                Text("Speed Radar")
+                    .bold()
+                
+                Spacer()
+
+            }
+            
+            // Input: Speed limit
+            Slider(value: $speedLimit,
+                   in: 0...200,
+                   step: 0.1,
+                   label: {
+                Text("Speed limit")
+            },
+                   minimumValueLabel: {
+                Text("0")
+            },
+                   maximumValueLabel: {
+                Text("200")
+            })
+            
+            HStack {
+                Spacer()
+                Text("\(String(format: "%.2f", speedLimit))")
+                    .font(.title2)
+                    .bold()
+                Spacer()
+            }
+            
+            // Input: Speed travelled
+            Slider(value: $speedTravelled,
+                   in: 0...200,
+                   step: 0.1,
+                   label: {
+                Text("Speed travelled")
+            },
+                   minimumValueLabel: {
+                Text("0")
+            },
+                   maximumValueLabel: {
+                Text("200")
+            })
+        
+            
+            //Output:Provide feedback on their fine
+            Text(feedback)
+            
+            Spacer()
+            
+        }
+        .padding()
+        .navigationTitle("Speed radar")
+
     }
 }
 
